efce2949b1f9f3b9ac8b1fb23c8523e57c50d743
Bgabor-prog <demiteto.bartok@gmail.com>
2022-03-02 19:17:42 +0100

    Prefix

---
+++ Gyakorlat2/Gyak2Prefix/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int arr[] = [4,7,2,1,6,5,3,5];
+    int prefix[] = [4,11,13,14,20,25,28,33];
+
+    printf("Hello world!\n");
+    return 0;
+}
